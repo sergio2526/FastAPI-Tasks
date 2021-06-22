c9ea0994c760d74b022352441fa598b17aae129c
sergio2526 <sergiorubiano25@hotmail.com>
2021-06-22 17:14:31 -0500

    Agregando archivos

---
+++ main.py
@@ -0,0 +1,30 @@
+import os
+from dotenv import load_dotenv
+from fastapi import FastAPI
+from google.cloud import storage
+
+
+load_dotenv()
+
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = os.getenv('CREDENTIALS')
+app = FastAPI()
+
+@app.get('/')
+def hello():
+    return {"Hello" : "Friend"}
+
+@app.get('/pull_scv')
+def pull_csv():
+    
+    bucket_name = os.getenv('BUCKET_NAME')
+    source_blob_name = os.getenv('SOURCE_BLOB_NAME')
+    destination_file_name = os.getenv('DESTINATION_FILE_NAME')
+
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+
+    blob = bucket.blob(source_blob_name)
+    blob.download_to_filename(destination_file_name)
+
+
+    return {f'Blob {source_blob_name} downloaded to {destination_file_name}' }
